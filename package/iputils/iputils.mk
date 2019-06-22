tecmx/LabSisop-BuildRoot.x

commit 2f71c5c777aeec6811a142e2dba043efb930a458

package/iputils: bump to version s20190515
This commit also include changes from s20190324.

Upstream changed build system to Meson, these extra changes
were required:
* remove custom commands of previous build system:
(IPUTILS_INSTALL_TARGET_CMDS, IPUTILS_PERMISSIONS)
* remove USE_SYSFS=no (support removed from upstream
* transform options from USE_FOO={yes,no} to -DUSE_FOO={true,false}

Removed patch included in this release

Build ninfod if possible (when use any crypto)

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/iputils/iputils.mk
@@ -11,61 +11,45 @@
 # and IPv6 updates.
 # http://www.spinics.net/lists/netdev/msg279881.html
 
-IPUTILS_VERSION = s20180629
+IPUTILS_VERSION = s20190515
 IPUTILS_SITE = $(call github,iputils,iputils,$(IPUTILS_VERSION))
 IPUTILS_LICENSE = GPL-2.0+, BSD-3-Clause, BSD-4-Clause
 # Only includes a license file for BSD
 IPUTILS_LICENSE_FILES = ninfod/COPYING
 
-IPUTILS_MAKE_OPTS = $(TARGET_CONFIGURE_OPTS) USE_SYSFS=no USE_IDN=no\
-	CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
-
 ifeq ($(BR2_PACKAGE_LIBCAP),y)
-IPUTILS_MAKE_OPTS += USE_CAP=yes
+IPUTILS_CONF_OPTS += -DUSE_CAP=true
 IPUTILS_DEPENDENCIES += libcap
 else
-IPUTILS_MAKE_OPTS += USE_CAP=no
+IPUTILS_CONF_OPTS += -DUSE_CAP=false
 endif
 
-ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
-IPUTILS_MAKE_OPTS += USE_GCRYPT=yes
-IPUTILS_DEPENDENCIES += libgcrypt
+ifeq ($(BR2_PACKAGE_LIBIDN),y)
+IPUTILS_CONF_OPTS += -DUSE_IDN=true
+IPUTILS_DEPENDENCIES += libidn
 else
-IPUTILS_MAKE_OPTS += USE_GCRYPT=no
+IPUTILS_CONF_OPTS += -DUSE_IDN=false
 endif
 
 ifeq ($(BR2_PACKAGE_NETTLE),y)
-IPUTILS_MAKE_OPTS += USE_NETTLE=yes
+IPUTILS_CONF_OPTS += -DUSE_CRYPTO=nettle
 IPUTILS_DEPENDENCIES += nettle
-else
-IPUTILS_MAKE_OPTS += USE_NETTLE=no
-endif
-
-ifeq ($(BR2_PACKAGE_OPENSSL),y)
-IPUTILS_MAKE_OPTS += USE_CRYPTO=yes
+else ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
+IPUTILS_CONF_OPTS += -DUSE_CRYPTO=gcrypt
+IPUTILS_DEPENDENCIES += libgcrypt
+else ifeq ($(BR2_PACKAGE_OPENSSL),y)
+IPUTILS_CONF_OPTS += -DUSE_CRYPTO=openssl
 IPUTILS_DEPENDENCIES += openssl
+else ifeq ($(BR2_PACKAGE_LINUX_HEADERS),y)
+IPUTILS_CONF_OPTS += -DUSE_CRYPTO=kernel
+IPUTILS_DEPENDENCIES += linux-headers
 else
-IPUTILS_MAKE_OPTS += USE_CRYPTO=no
+IPUTILS_CONF_OPTS += -DUSE_CRYPTO=none
+# BUILD_NINFOD=true and USE_CRYPTO=none cannot be combined
+IPUTILS_CONF_OPTS += -DBUILD_NINFOD=false
 endif
 
-define IPUTILS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(IPUTILS_MAKE_OPTS)
-endef
+# XSL Stylesheets for DocBook 5 not packaged for buildroot
+IPUTILS_CONF_OPTS += -DBUILD_MANS=false -DBUILD_HTML_MANS=false
 
-define IPUTILS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/arping      $(TARGET_DIR)/sbin/arping
-	$(INSTALL) -D -m 755 $(@D)/clockdiff   $(TARGET_DIR)/bin/clockdiff
-	$(INSTALL) -D -m 755 $(@D)/ping        $(TARGET_DIR)/bin/ping
-	$(INSTALL) -D -m 755 $(@D)/rarpd       $(TARGET_DIR)/sbin/rarpd
-	$(INSTALL) -D -m 755 $(@D)/rdisc       $(TARGET_DIR)/sbin/rdisc
-	$(INSTALL) -D -m 755 $(@D)/tftpd       $(TARGET_DIR)/usr/sbin/in.tftpd
-	$(INSTALL) -D -m 755 $(@D)/tracepath   $(TARGET_DIR)/bin/tracepath
-	$(INSTALL) -D -m 755 $(@D)/traceroute6 $(TARGET_DIR)/bin/traceroute6
-endef
-
-define IPUTILS_PERMISSIONS
-	/bin/ping        f 4755 0 0 - - - - -
-	/bin/traceroute6 f 4755 0 0 - - - - -
-endef
-
-$(eval $(generic-package))
+$(eval $(meson-package))

package/iputils/0001-ping-Fix-AI_CANONIDN-usage-on-some-systems.patch
@@ -1,57 +1,0 @@
-From 25899e849aa3abc1ad29ebf0b830262a859eaed5 Mon Sep 17 00:00:00 2001
-From: Petr Vorel <petr.vorel@gmail.com>
-Date: Sat, 21 Jul 2018 17:46:14 +0200
-Subject: [PATCH] ping: Fix AI_CANONIDN usage on some systems
-
-Commit 99f67db used AI_CANONIDN in a way, which broke compilation on
-systems where AI_CANONIDN is not defined in netdb.h (e.g. glibc < 2.3.4,
-alternative libcs that don't support IDN: e.g. current musl 1.1.19 and
-uClibc-ng 1.0.30) when not using the system libidn2.
-
-Fixes: 99f67db ping: Fix ping name encoded using ACE on C locale
-
-Reported-by: Nicholas Fish
-Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
----
- ping.c | 2 +-
- ping.h | 2 +-
- 2 files changed, 2 insertions(+), 2 deletions(-)
-
-diff --git a/ping.c b/ping.c
-index 733477f..b241815 100644
---- a/ping.c
-+++ b/ping.c
-@@ -207,9 +207,9 @@ main(int argc, char **argv)
- 
- #ifdef USE_IDN
- 	setlocale(LC_ALL, "");
--#endif
- 	if (!strcmp(setlocale(LC_ALL, NULL), "C"))
- 		hints.ai_flags &= ~ AI_CANONIDN;
-+#endif
- 
- 	/* Support being called using `ping4` or `ping6` symlinks */
- 	if (argv[0][strlen(argv[0])-1] == '4')
-diff --git a/ping.h b/ping.h
-index 3e09685..8a0c4ef 100644
---- a/ping.h
-+++ b/ping.h
-@@ -28,7 +28,6 @@
- #include <netinet/icmp6.h>
- #include <linux/filter.h>
- #include <resolv.h>
--#include <locale.h>
- 
- #ifdef CAPABILITIES
- #include <sys/prctl.h>
-@@ -36,6 +35,7 @@
- #endif
- 
- #ifdef USE_IDN
-+#include <locale.h>
- #include <idn2.h>
- #define getaddrinfo_flags (AI_CANONNAME | AI_IDN | AI_CANONIDN)
- #define getnameinfo_flags NI_IDN
--- 
-2.19.0.rc2
-

package/iputils/iputils.hash
@@ -1,3 +1,3 @@
 # Locally computed
-sha256 da14105291dd491f28ea91ade854ed10aee8ba019641c80eed233de3908be7c5  iputils-s20180629.tar.gz
+sha256 9b5125eb5ef9f4e947ad8fdddcf77f538f53b8f47b53eb5bc5347cb16d01c8fd  iputils-s20190515.tar.gz
 sha256 966075293e45785230c19415bcda15bd07c75bead4fe73332d1a4b9c45bb321f  ninfod/COPYING

commit d2d88aa2967a82b9f9393797dcfee1b39d739c9c

package/iputils: re-enable traceroute6
Bump to s20190515 removed traceroute6 build (it's not built by default
since this version in upstream). Reenable it again.

Fixes: 2f71c5c777 ("package/iputils: bump to version s20190515")

This requires fix similar to one in 0003-meson.build-fix-build-with-NLS.patch [1]

traceroute6 is in /usr/sbin, so no need to handle busybox problem
(48a0063cb5 "package/iputils: move binaries to the location also used by
Busybox").

[1] http://autobuild.buildroot.org/results/0a8a3efe734ac7fb3a68ba505277681857dc0a3d

Suggested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/iputils/0005-traceroute6-fix-build-with-NLS.patch
@@ -1,0 +1,30 @@
+From b18c6c48e4dabaf8bf9b178b3deca631fc79c96e Mon Sep 17 00:00:00 2001
+From: Petr Vorel <pvorel@suse.cz>
+Date: Tue, 11 Jun 2019 21:51:58 +0200
+Subject: [PATCH] traceroute6: fix build with NLS
+
+commit 418d8be ("build-sys: fix build with NLS") fixed problems for
+default packages using libintl. Fix also traceroute6 (build it must be
+enabled with -DBUILD_TRACEROUTE6=true)
+
+Signed-off-by: Petr Vorel <pvorel@suse.cz>
+Upstream status: 81bfb31a714641b70c2a7c7d9ce12cf08f98a372
+---
+ meson.build | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/meson.build b/meson.build
+index 2b545b6..85db8a6 100644
+--- a/meson.build
++++ b/meson.build
+@@ -266,7 +266,7 @@ endif
+
+ if build_traceroute6 == true
+	executable('traceroute6', ['traceroute6.c', git_version_h],
+-		dependencies : [cap_dep, idn_dep],
++		dependencies : [cap_dep, intl_dep, idn_dep],
+		link_with : [libcommon],
+		install: true)
+	meson.add_install_script('build-aux/setcap-setuid.sh',
+--
+2.21.0

package/iputils/iputils.mk
@@ -49,6 +49,8 @@ IPUTILS_CONF_OPTS += -DUSE_CRYPTO=none
 IPUTILS_CONF_OPTS += -DBUILD_NINFOD=false
 endif
 
+IPUTILS_CONF_OPTS += -DBUILD_TRACEROUTE6=true
+
 # XSL Stylesheets for DocBook 5 not packaged for buildroot
 IPUTILS_CONF_OPTS += -DBUILD_MANS=false -DBUILD_HTML_MANS=false
 
@@ -69,6 +71,7 @@ define IPUTILS_PERMISSIONS
 	/usr/sbin/arping      f 4755 0 0 - - - - -
 	/usr/bin/clockdiff    f 4755 0 0 - - - - -
 	/bin/ping             f 4755 0 0 - - - - -
+	/usr/bin/traceroute6  f 4755 0 0 - - - - -
 endef
 
 $(eval $(meson-package))