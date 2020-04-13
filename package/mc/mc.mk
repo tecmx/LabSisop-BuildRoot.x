tecmx/LabSisop-BuildRoot.x

commit 8586ba10584811bf2aee160805bae75a3f991b6e

package/mc: add optional dependency to gmp
Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

package/mc/mc.mk
@@ -11,6 +11,13 @@ MC_LICENSE = GPL-3.0+
 MC_LICENSE_FILES = COPYING
 MC_DEPENDENCIES = libglib2 host-pkgconf
 
+ifeq ($(BR2_PACKAGE_GMP),y)
+MC_CONF_OPTS += --with-gpm-mouse
+MC_DEPENDENCIES += gpm
+else
+MC_CONF_OPTS += --without-gpm-mouse
+endif
+
 ifeq ($(BR2_PACKAGE_LIBSSH2),y)
 MC_CONF_OPTS += --enable-vfs-sftp
 MC_DEPENDENCIES += libssh2

commit 250199c43eb4084021dcf831606d709fc09fd240

package/mc: fix typo BR2_PACKAGE_GMP -> BR2_PACKAGE_GPM
Commit 8586ba10584811bf2aee160805bae75a3f991b6e introduced an optional
dependency on GPM, but got the name of the option wrong, and used
GMP. In fact, even the commit title was wrong.

This causes a build failure:

Makefile:578: *** gpm is in the dependency chain of mc that has added it to its _DEPENDENCIES variable without selecting it or depending on it from Config.in.  Stop.

Fixes:

  http://autobuild.buildroot.net/results/52fb92ae7dd55cba7d19862bb6cd89c80da9a4b6/

Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/mc/mc.mk
@@ -11,7 +11,7 @@ MC_LICENSE = GPL-3.0+
 MC_LICENSE_FILES = COPYING
 MC_DEPENDENCIES = libglib2 host-pkgconf
 
-ifeq ($(BR2_PACKAGE_GMP),y)
+ifeq ($(BR2_PACKAGE_GPM),y)
 MC_CONF_OPTS += --with-gpm-mouse
 MC_DEPENDENCIES += gpm
 else