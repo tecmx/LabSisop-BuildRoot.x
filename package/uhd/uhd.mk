tecmx/LabSisop-BuildRoot.x

commit f9ea87d4fff33fe1756edb84405bb2010af6eda5

package/python-markupsafe: bump to version 2.0.1
Drop python2 support.

Remove python3-markupsafe host build as python2 host builds for
markupsafe are not supported, python3 host builds are moved to
python-markupsafe.

Remove python3-jinja2 host build as python2 host builds for
jinja2 are not supported, python3 host builds are moved to
python-jinja2.

Remove python3-mako host build as python2 host builds for
mako are not supported, python3 host builds are moved to
python-mako.

Propagate reverse python3 dependency.

Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

package/uhd/uhd.mk
@@ -15,7 +15,7 @@ UHD_INSTALL_STAGING = YES
 
 UHD_DEPENDENCIES = \
 	boost \
-	$(if $(BR2_PACKAGE_PYTHON),host-python,host-python3) \
+	host-python3 \
 	host-python-mako
 
 UHD_CONF_OPTS = \

package/gnuradio/gnuradio.mk
@@ -20,7 +20,7 @@ endif
 
 # host-python-mako and host-python-six are needed for volk to compile
 GNURADIO_DEPENDENCIES = \
-	$(if $(BR2_PACKAGE_PYTHON3),host-python3,host-python) \
+	host-python3 \
 	host-python-mako \
 	host-python-six \
 	host-swig \

package/jailhouse/Config.in
@@ -11,15 +11,15 @@ if BR2_PACKAGE_JAILHOUSE
 
 config BR2_PACKAGE_JAILHOUSE_HELPER_SCRIPTS
 	bool "helper scripts"
-	depends on BR2_PACKAGE_PYTHON
+	depends on BR2_PACKAGE_PYTHON3
 	select BR2_PACKAGE_PYTHON_MAKO # runtime
 	help
 	  Python-based helpers for the Jailhouse Hypervisor.
 
 	  https://github.com/siemens/jailhouse
 
-comment "helper scripts require Python"
-	depends on !BR2_PACKAGE_PYTHON
+comment "helper scripts require Python3"
+	depends on !BR2_PACKAGE_PYTHON3
 
 endif
 

package/libcamera/libcamera.mk
@@ -10,7 +10,7 @@ LIBCAMERA_SITE_METHOD = git
 LIBCAMERA_DEPENDENCIES = \
 	host-openssl \
 	host-pkgconf \
-	host-python3-jinja2 \
+	host-python-jinja2 \
 	host-python3-ply \
 	host-python3-pyyaml \
 	gnutls

package/mesa3d/mesa3d.mk
@@ -20,7 +20,7 @@ MESA3D_PROVIDES =
 MESA3D_DEPENDENCIES = \
 	host-bison \
 	host-flex \
-	host-python3-mako \
+	host-python-mako \
 	expat \
 	libdrm \
 	zlib

package/python-flask-babel/Config.in
@@ -1,5 +1,6 @@
 config BR2_PACKAGE_PYTHON_FLASK_BABEL
 	bool "python-flask-babel"
+	depends on BR2_PACKAGE_PYTHON3
 	select BR2_PACKAGE_PYTHON_FLASK # runtime
 	select BR2_PACKAGE_PYTHON_BABEL # runtime
 	select BR2_PACKAGE_PYTHON_JINJA2 # runtime

package/python-flask-cors/Config.in
@@ -1,5 +1,6 @@
 config BR2_PACKAGE_PYTHON_FLASK_CORS
 	bool "python-flask-cors"
+	depends on BR2_PACKAGE_PYTHON3
 	select BR2_PACKAGE_PYTHON_FLASK # runtime
 	select BR2_PACKAGE_PYTHON_SIX # runtime
 	help

package/python-flask-expects-json/Config.in
@@ -1,5 +1,6 @@
 config BR2_PACKAGE_PYTHON_FLASK_EXPECTS_JSON
 	bool "python-flask-expects-json"
+	depends on BR2_PACKAGE_PYTHON3
 	select BR2_PACKAGE_PYTHON_FLASK # runtime
 	select BR2_PACKAGE_PYTHON_JSONSCHEMA # runtime
 	help

commit d199c87c71af0addca905b68b08d1d6f2c7b404c

package/uhd: fix python module detection
$(HOST_DIR)/bin/python symlink is only created when
BR2_PACKAGE_PYTHON3 is set.

When only host-python3 is used this symlink is not present and build fails
with:
-- Python checking for Python version 2.7 or greater
-- Python checking for Python version 2.7 or greater - unknown error
--
-- Python checking for Mako templates 0.4.2 or greater
-- Python checking for Mako templates 0.4.2 or greater - unknown error
--
-- Python checking for requests 2.0 or greater
-- Python checking for requests 2.0 or greater - unknown error
--
-- Python checking for numpy 1.7 or greater
-- Python checking for numpy 1.7 or greater - unknown error
--
-- Configuring LibUHD support...
--   Dependency Boost_FOUND = TRUE
--   Dependency HAVE_PYTHON_PLAT_MIN_VERSION = FALSE
--   Dependency HAVE_PYTHON_MODULE_MAKO = FALSE

This patch set explicitly PYTHON_EXECUTABLE to $(HOST_DIR)/bin/python3

This is a fallout from f9ea87d4fff33fe1756edb84405bb2010af6eda5, which
changed uhd to always use host-python3.

Fixes:
- http://autobuild.buildroot.net/results/35c0b9597c75dade241a3b3786fae30551c48008
- http://autobuild.buildroot.net/results/89069102bf8785ce8a9ec3b4c6fa32749c2e3586

Signed-off-by: Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/uhd/uhd.mk
@@ -19,7 +19,7 @@ UHD_DEPENDENCIES = \
 	host-python-mako
 
 UHD_CONF_OPTS = \
-	-DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python \
+	-DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python3 \
 	-DRUNTIME_PYTHON_EXECUTABLE=/usr/bin/python \
 	-DENABLE_C_API=ON \
 	-DENABLE_DOXYGEN=OFF \