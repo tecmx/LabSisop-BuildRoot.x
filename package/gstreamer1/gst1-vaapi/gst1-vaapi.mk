tecmx/LabSisop-BuildRoot.x

commit ef921bbcc30bf175300190d54e7fd42a23d50e64

package/gst1-vaapi: bump version to 1.22.0
Update config option names and dependency requirements.

Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/gstreamer1/gst1-vaapi/gst1-vaapi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_VAAPI_VERSION = 1.20.4
+GST1_VAAPI_VERSION = 1.22.0
 GST1_VAAPI_SITE = https://gstreamer.freedesktop.org/src/gstreamer-vaapi
 GST1_VAAPI_SOURCE = gstreamer-vaapi-$(GST1_VAAPI_VERSION).tar.xz
 GST1_VAAPI_LICENSE = LGPL-2.1+
@@ -18,19 +18,40 @@ GST1_VAAPI_DEPENDENCIES += \
 	libdrm
 
 GST1_VAAPI_CONF_OPTS += \
-	-Dwith_drm=yes \
-	-Dwith_x11=no \
-	-Dwith_glx=no \
-	-Dwith_wayland=no \
-	-Dwith_egl=no \
+	-Ddrm=enabled \
+	-Degl=enabled \
 	-Dexamples=disabled \
 	-Dtests=disabled \
 	-Ddoc=disabled
 
 ifeq ($(BR2_PACKAGE_GST1_VAAPI_ENCODERS),y)
-GST1_VAAPI_CONF_OPTS += -Dwith_encoders=yes
+GST1_VAAPI_CONF_OPTS += -Dencoders=enabled
 else
-GST1_VAAPI_CONF_OPTS += -Dwith_encoders=no
+GST1_VAAPI_CONF_OPTS += -Dencoders=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
+GST1_VAAPI_CONF_OPTS += -Degl=enabled
+GST1_VAAPI_DEPENDENCIES += libegl
+else
+GST1_VAAPI_CONF_OPTS += -Degl=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_WAYLAND),y)
+GST1_VAAPI_CONF_OPTS += -Dwayland=enabled
+else
+GST1_VAAPI_CONF_OPTS += -Dwayland=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+GST1_VAAPI_CONF_OPTS += -Dx11=enabled
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+GST1_VAAPI_CONF_OPTS += -Dglx=enabled
+else
+GST1_VAAPI_CONF_OPTS += -Dglx=disabled
+endif
+else
+GST1_VAAPI_CONF_OPTS += -Dx11=disabled -Dglx=disabled
 endif
 
 $(eval $(meson-package))

package/gstreamer1/gst1-vaapi/gst1-vaapi.hash
@@ -1,3 +1,3 @@
-# From https://gstreamer.freedesktop.org/src/gstreamer-vaapi/gstreamer-vaapi-1.20.4.tar.xz.sha256sum
-sha256  ab12596144c05506e9782374c5d2cdfb3069fca89908d6de360d947bb77fd06a  gstreamer-vaapi-1.20.4.tar.xz
+# From https://gstreamer.freedesktop.org/src/gstreamer-vaapi/gstreamer-vaapi-1.22.0.tar.xz.sha256sum
+sha256  593ccad19f88e5fa29f40f98356c007806bd535828707b1406944d16a90bdff5  gstreamer-vaapi-1.22.0.tar.xz
 sha256  dc626520dcd53a22f727af3ee42c770e56c97a64fe3adb063799d8ab032fe551  COPYING.LIB

commit 0730b0473a2c27c1ee1235f65f530cdd25731dca

package/gstreamer1/gst1-vaapi: fix wayland build
wayland-protocols is a mandatory dependency with wayland since
bump to version 1.22.0 in commit
ef921bbcc30bf175300190d54e7fd42a23d50e64 and
https://gitlab.freedesktop.org/gstreamer/gstreamer/-/commit/3c713cc16ee1d374213cb2b47a3127e14e5ff8a5:

Run-time dependency wayland-protocols found: NO (tried pkgconfig and cmake)

../output-1/build/gst1-vaapi-1.22.0/meson.build:111:0: ERROR: Dependency "wayland-protocols" not found, tried pkgconfig and cmake

Fixes: ef921bbcc30bf175300190d54e7fd42a23d50e64
 - http://autobuild.buildroot.org/results/b5e79d4e10fdf24c3fd43ab742d9d14a5d07824b
 - http://autobuild.buildroot.org/results/ad624cd11d5a1b3346974000ae2b61f4261af02c

Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Arnout Vandecappelle <arnout@mind.be>
(cherry picked from commit be71f60f098f3f7ddf047c5e30853c0acd25d130)
Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

package/gstreamer1/gst1-vaapi/Config.in
@@ -8,6 +8,7 @@ config BR2_PACKAGE_GST1_VAAPI
 	select BR2_PACKAGE_LIBDRM
 	select BR2_PACKAGE_GST1_PLUGINS_BASE
 	select BR2_PACKAGE_GST1_PLUGINS_BAD # gstreamer-codecparsers
+	select BR2_PACKAGE_WAYLAND_PROTOCOLS if BR2_PACKAGE_WAYLAND
 	select BR2_PACKAGE_XLIB_LIBX11 if BR2_PACKAGE_XORG7
 	select BR2_PACKAGE_XLIB_LIBXRANDR if BR2_PACKAGE_XORG7
 	help

package/gstreamer1/gst1-vaapi/gst1-vaapi.mk
@@ -39,6 +39,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_WAYLAND),y)
 GST1_VAAPI_CONF_OPTS += -Dwayland=enabled
+GST1_VAAPI_DEPENDENCIES += wayland wayland-protocols
 else
 GST1_VAAPI_CONF_OPTS += -Dwayland=disabled
 endif